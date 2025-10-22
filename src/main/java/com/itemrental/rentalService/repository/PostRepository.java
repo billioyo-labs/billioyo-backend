a8225c27cb62f172f97d9e8eaaf9df6b8eb7dc2e
syeon111 <psooy19@gmail.com>
2025-10-22 20:05:35 +0900

    feat: RentalPost 목록 응답 Service(getPosts) 추가

--- src/main/java/com/itemrental/rentalService/repository/PostRepository.java
+++ src/main/java/com/itemrental/rentalService/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.itemrental.rentalService.repository;
 
+import com.itemrental.rentalService.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }

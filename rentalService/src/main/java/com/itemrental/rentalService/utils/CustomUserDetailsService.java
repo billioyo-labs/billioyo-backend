3d696d946d54b4e4aa2b503602814c6070dab831
eunki <bada4136@naver.com>
2025-10-22 21:37:55 +0900

    feat:ip에서 도메인으로 요청 url 변경, 로그인 시 user 정보 반환

--- rentalService/src/main/java/com/itemrental/rentalService/utils/CustomUserDetailsService.java
+++ rentalService/src/main/java/com/itemrental/rentalService/utils/CustomUserDetailsService.java
@@ -16,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(()->new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
         if(user.getUserState() != User.UserState.ACTIVE){
             throw new DisabledException("계정이 활성화 상태가 아닙니다.");
@@ -27,6 +27,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserDetails createUserDetail(User user){
         return User.builder()
                 .username(user.getUsername())
+                .email(user.getEmail())
                 .password(user.getPassword())
                 .roles(user.getRoles())
                 .build();

a25b46a3b3e0d6330b09809897c9c6f7ec46f3b7
syeon111 <psooy19@gmail.com>
2025-11-19 21:24:04 +0900

    feat: PostRepository에 userId로 검색하는 메서드 추가 및 페이징 기능 구현

--- src/main/java/com/itemrental/rentalService/rental/repository/PostRepository.java
+++ src/main/java/com/itemrental/rentalService/rental/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.itemrental.rentalService.rental.repository;
 
 import com.itemrental.rentalService.rental.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+  Page<Post> findByUserId(Long userId, Pageable pageable);
 }
